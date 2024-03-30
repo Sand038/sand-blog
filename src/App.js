c39e3bfb156d18684de18cc5140d3b35eb3a970c
Sand038 <sandmdh@gmail.com>
2024-03-30 19:09:46 +0800

    added json data and fetched using json-server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Home from './Home';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="content">
         <Home />
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    return ( 
+    const [blogs, setBlogs] = useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+            })
+    }, [])
+
+    return (
         <div className="home">
-            <h2>Home Page</h2>
+            {blogs && <BlogList blogs={blogs} />}
         </div>
-     );
+    );
 }
- 
+
+
 export default Home;
