d3ee523fd6194a7cfeb10f8e07e8fe43df6bcbb5
Sand038 <sandmdh@gmail.com>
2024-03-31 11:37:38 +0800

    added routing functionalites

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import Navbar from './Navbar';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import BlogDetails from './BlogDetails';
+import Create from './Create';
 import Home from './Home';
+import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
